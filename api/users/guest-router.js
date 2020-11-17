58461480e9d35699cf06cfee8abf1d0b32861982
Raul <raulefloresjr@gmail.com>
2020-11-16 18:06:42 -0800

    for push 3

--- api/users/guest-router.js
+++ api/users/guest-router.js
@@ -9,12 +9,12 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req,res, next) => {
-    try {
-        const 
-    } catch (error) {
-        next(error)
-    }
-})
+// router.get("/:id", async (req,res, next) => {
+//     try {
+//         const
+//     } catch (error) {
+//         next(error)
+//     }
+// })
 
 module.exports = router;
